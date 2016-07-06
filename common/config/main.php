740d98ee51c43d6da2c822da9ab3b52137702488
ioioj5 <peng_du2007@qq.com>
2016-07-06 18:54:58 +0800

    add config file

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=tool.local',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
