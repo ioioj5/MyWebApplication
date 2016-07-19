118313190f2f255b9a553e65fa72cdefabd2bb15
ioioj5 <peng_du2007@qq.com>
2016-07-19 15:46:30 +0800

    add vendor

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,41 +1,51 @@
 <?php
-$params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+$params = array_merge (
+	require ( __DIR__ . '/../../common/config/params.php' ),
+	require ( __DIR__ . '/../../common/config/params-local.php' ),
+	require ( __DIR__ . '/params.php' ),
+	require ( __DIR__ . '/params-local.php' )
 );
 
 return [
-    'id' => 'app-frontend',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'frontend\controllers',
-    'components' => [
-        'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
-    ],
-    'params' => $params,
+	'id'                  => 'app-frontend',
+	'basePath'            => dirname ( __DIR__ ),
+	'bootstrap'           => [ 'log' ],
+	'defaultRoute' => 'site', // 默认控制器
+	'controllerNamespace' => 'frontend\controllers',
+	'components'          => [
+		'user'         => [
+			'identityClass'   => 'common\models\User',
+			'enableAutoLogin' => true,
+		],
+		'log'          => [
+			'traceLevel' => YII_DEBUG ? 3 : 0,
+			'targets'    => [
+				[
+					'class'  => 'yii\log\FileTarget',
+					'levels' => [ 'error', 'warning' ],
+				],
+			],
+		],
+		'errorHandler' => [
+			'errorAction' => 'site/error',
+		],
+		'urlManager'   => [
+			'scriptUrl'       => '/admini/index.php',
+			'enablePrettyUrl' => true,
+			'showScriptName'  => false,
+			'rules'           => [
+				//'index' => 'site/index',
+			]
+		],
+
+		/*
+		'urlManager' => [
+			'enablePrettyUrl' => true,
+			'showScriptName' => false,
+			'rules' => [
+			],
+		],
+		*/
+	],
+	'params'              => $params,
 ];
