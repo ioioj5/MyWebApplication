4e18edcd6fdc6f046e2d04020e897867d2b7d058
ioioj5 <peng_du2007@qq.com>
2016-07-20 18:21:05 +0800

    初始化程序

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -2,50 +2,54 @@
 $params = array_merge (
 	require ( __DIR__ . '/../../common/config/params.php' ),
 	require ( __DIR__ . '/../../common/config/params-local.php' ),
-	require ( __DIR__ . '/params.php' ),
-	require ( __DIR__ . '/params-local.php' )
+	require ( __DIR__ . '/params.php' ), require ( __DIR__ . '/params-local.php' )
 );
 
 return [
 	'id'                  => 'app-frontend',
 	'basePath'            => dirname ( __DIR__ ),
-	'bootstrap'           => [ 'log' ],
-	'defaultRoute' => 'site', // 默认控制器
+	'bootstrap'           => [
+		'log'
+	],
+	'defaultRoute'        => 'site', // 默认控制器
 	'controllerNamespace' => 'frontend\controllers',
 	'components'          => [
 		'user'         => [
 			'identityClass'   => 'common\models\User',
-			'enableAutoLogin' => true,
+			'enableAutoLogin' => true
 		],
 		'log'          => [
 			'traceLevel' => YII_DEBUG ? 3 : 0,
 			'targets'    => [
 				[
 					'class'  => 'yii\log\FileTarget',
-					'levels' => [ 'error', 'warning' ],
-				],
-			],
+					'levels' => [
+						'error',
+						'warning'
+					]
+				]
+			]
 		],
 		'errorHandler' => [
-			'errorAction' => 'site/error',
+			'errorAction' => 'site/error'
 		],
 		'urlManager'   => [
 			'scriptUrl'       => '/admini/index.php',
 			'enablePrettyUrl' => true,
 			'showScriptName'  => false,
-			'rules'           => [
-				//'index' => 'site/index',
-			]
-		],
+			'rules'           => [ ]
+		]
+	]
+	// 'index' => 'site/index',
 
-		/*
-		'urlManager' => [
-			'enablePrettyUrl' => true,
-			'showScriptName' => false,
-			'rules' => [
-			],
-		],
-		*/
-	],
-	'params'              => $params,
+	/*
+	 * 'urlManager' => [
+	 * 'enablePrettyUrl' => true,
+	 * 'showScriptName' => false,
+	 * 'rules' => [
+	 * ],
+	 * ],
+	 */
+	,
+	'params'              => $params
 ];
