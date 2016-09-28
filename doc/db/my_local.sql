b1c148d335d8cbc6f0a094aec804c9196d4b581a
ioioj5 <peng_du2007@qq.com>
2016-09-29 02:14:28 +0800

    add db

---
+++ doc/db/my_local.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 2016-09-29 02:13:48
+-- 服务器版本： 5.6.30
+-- PHP Version: 5.5.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my.local`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_goods`
+--
+
+CREATE TABLE `tbl_goods` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL COMMENT '商品名称',
+  `price` decimal(5,0) NOT NULL COMMENT '商品价格',
+  `postTime` int(10) NOT NULL COMMENT '添加时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL COMMENT '用户id',
+  `orderCode` char(12) NOT NULL COMMENT '订单号',
+  `price` decimal(5,0) NOT NULL COMMENT '订单价格',
+  `payStatus` tinyint(1) NOT NULL COMMENT '支付状态',
+  `orderStatus` tinyint(1) NOT NULL COMMENT '订单状态',
+  `payTime` int(10) NOT NULL COMMENT '支付时间',
+  `postTime` int(10) NOT NULL COMMENT '订单生成时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_order_address`
+--
+
+CREATE TABLE `tbl_order_address` (
+  `id` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL COMMENT '订单id',
+  `name` varchar(50) NOT NULL COMMENT '联系人',
+  `contact` varchar(50) NOT NULL COMMENT '联系方式(电话,qq,email)',
+  `address` int(11) NOT NULL COMMENT '收货地址',
+  `postTime` int(10) NOT NULL COMMENT '添加时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户收获地址';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_order_log`
+--
+
+CREATE TABLE `tbl_order_log` (
+  `id` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL COMMENT '订单id',
+  `orderStatus` tinyint(1) NOT NULL COMMENT '订单状态',
+  `postTime` int(10) NOT NULL COMMENT '添加时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单日志表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL COMMENT '登入邮箱',
+  `password` char(64) NOT NULL COMMENT '密码',
+  `token` char(64) NOT NULL,
+  `regtime` int(10) NOT NULL COMMENT '注册时间',
+  `logintime` int(10) NOT NULL DEFAULT '0' COMMENT '登入时间',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '用户状态(1-正常,0-关闭)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+--
+-- 转存表中的数据 `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `email`, `password`, `token`, `regtime`, `logintime`, `status`) VALUES
+(1, 'peng_du2007@qq.com', '$2a$15$C81gSdUkgmzt4Y23hVucjOeI66S8zOq21PIzaCfcchgoSfG51TxNe', 'RmFldUtWc0sMKndv-zgfbjFdFl-0_gZq', 1469202625, 1469202625, 1);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_user_address`
+--
+
+CREATE TABLE `tbl_user_address` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL COMMENT '用户id',
+  `name` varchar(50) NOT NULL COMMENT '联系人',
+  `contact` varchar(50) NOT NULL COMMENT '联系方式(电话,qq,email)',
+  `address` int(11) NOT NULL COMMENT '收货地址',
+  `isDefault` tinyint(1) NOT NULL COMMENT '是否默认地址',
+  `postTime` int(10) NOT NULL COMMENT '添加时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户收获地址';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_goods`
+--
+ALTER TABLE `tbl_goods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_order_address`
+--
+ALTER TABLE `tbl_order_address`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_order_log`
+--
+ALTER TABLE `tbl_order_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user_address`
+--
+ALTER TABLE `tbl_user_address`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `tbl_goods`
+--
+ALTER TABLE `tbl_goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- 使用表AUTO_INCREMENT `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- 使用表AUTO_INCREMENT `tbl_order_address`
+--
+ALTER TABLE `tbl_order_address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- 使用表AUTO_INCREMENT `tbl_order_log`
+--
+ALTER TABLE `tbl_order_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- 使用表AUTO_INCREMENT `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- 使用表AUTO_INCREMENT `tbl_user_address`
+--
+ALTER TABLE `tbl_user_address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
