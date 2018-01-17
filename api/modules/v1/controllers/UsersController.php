832d1f0822d57ffe023c09a8f9555a74d871117a
ioioj5 <peng_du2007@qq.com>
2018-01-17 11:33:47 +0800

    add

--- api/modules/v1/controllers/UsersController.php
+++ api/modules/v1/controllers/UsersController.php
@@ -1,38 +1,37 @@
 <?php
+
 namespace api\modules\v1\controllers;
+
 use common\components\ApiBaseController;
 use Yii;
 use yii\web\NotFoundHttpException;
 
 class UsersController extends ApiBaseController {
-
 	/**
 	 * 登入
 	 */
-	public function actionSignIn(){
-		if(Yii::$app->request->isPost) {
-			$email = trim(Yii::$app->request->post('email'));
-			$password = trim(Yii::$app->request->post('password'));
+	public function actionSignIn () {
+		if ( Yii::$app->request->isPost ) {
+			$email    = trim ( Yii::$app->request->post ( 'email' ) );
+			$password = trim ( Yii::$app->request->post ( 'password' ) );
 
-			if(! empty($email)) {
+			if ( ! empty( $email ) ) {
 
-			}else {
-				throw new NotFoundHttpException("请填写邮箱.", 1);
+			} else {
+				throw new NotFoundHttpException( "请填写邮箱.", 1 );
 			}
-			if(! empty($password)) {
+			if ( ! empty( $password ) ) {
 
-			}else {
-				throw new NotFoundHttpException("请填写密码.", 1);
+			} else {
+				throw new NotFoundHttpException( "请填写密码.", 1 );
 			}
-
-
 		}
 	}
 
 	/**
 	 * 注册
 	 */
-	public function actionSignUp(){
+	public function actionSignUp () {
 
 	}
 }
